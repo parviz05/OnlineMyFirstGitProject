b791528adac05864f159c879e93e097b587548c1
Parviz Alasgarov <alas.parviz@gmail.com>
2019-08-17 07:03:32 -0500

    created new class

--- src/test/java/Test/NewClass.java
+++ src/test/java/Test/NewClass.java
@@ -1,5 +1,12 @@
 package Test;
 
+import org.testng.annotations.Test;
+
 public class NewClass {
 
+   @Test
+    protected void test1(){
+
+       System.out.println("Go to contactTest");
+    }
 }
